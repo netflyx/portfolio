865461d687da990906c46d0b200914ea716d5462
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-17 14:35:57 -0700

    Update global.js

--- global.js
+++ global.js
@@ -35,15 +35,6 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  // next step: create link and add it to nav
-}
-
-// Create link and add it to nav
-nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/netflyx/";         // GitHub Pages repo name
@@ -51,3 +42,16 @@ const BASE_PATH = (location.hostname === "localhost" || location.hostname === "1
   if (!url.startsWith('http')) {
     url = BASE_PATH + url;
   }
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  // Fix relative links for GitHub Pages
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  // Create and insert link into nav
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
+
+  // currentLink?.classList.add("current");
