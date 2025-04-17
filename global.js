448745678f5f2ee4930b325e38f397520819bea4
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-17 14:25:18 -0700

    edited nav in js

--- global.js
+++ global.js
@@ -22,3 +22,32 @@ const fallbackLink = navLinks.find(link => {
 // if (!currentLink && fallbackLink) {
 //   fallbackLink.classList.add("current");
 // }
+
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'cv.html', title: 'CV' },
+  { url: 'https://github.com/netflyx', title: 'GitHub' }
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+  // next step: create link and add it to nav
+}
+
+// Create link and add it to nav
+nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/netflyx/";         // GitHub Pages repo name
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
