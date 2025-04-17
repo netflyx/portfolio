c4857675a5a2c9a745f82dff49650ee05d075c18
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-17 14:44:08 -0700

    Update global.js

--- global.js
+++ global.js
@@ -1,66 +1,28 @@
-// console.log('IT’S ALIVE!');
-
-// function $$(selector, context = document) {
-//   return Array.from(context.querySelectorAll(selector));
-// }
-
-// const navLinks = $$("nav a");
-
-// let currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname,
-//   );
-
-// currentLink?.classList.add("current");
-
-// const currentPage = window.location.pathname.split("/").pop();
-
-// const fallbackLink = navLinks.find(link => {
-//   const linkPage = link.getAttribute("href").split("/").pop();
-//   return linkPage === currentPage;
-// });
-
-// // if (!currentLink && fallbackLink) {
-// //   fallbackLink.classList.add("current");
-// // }
-
-
-// let pages = [
-//   { url: '', title: 'Home' },
-//   { url: 'contact/', title: 'Contact' },
-//   { url: 'projects/', title: 'Projects' },
-//   { url: 'cv.html', title: 'CV' },
-//   { url: 'https://github.com/netflyx', title: 'GitHub' }
-// ];
-
-// let nav = document.createElement('nav');
-// document.body.prepend(nav);
-
-// const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-//   ? "/"                  // Local server
-//   : "/netflyx/";         // GitHub Pages repo name
-
-//   if (!url.startsWith('http')) {
-//     url = BASE_PATH + url;
-//   }
-
-// for (let p of pages) {
-//   let url = p.url;
-//   let title = p.title;
-
-//   // Fix relative links for GitHub Pages
-//   url = !url.startsWith('http') ? BASE_PATH + url : url;
-
-//   // Create and insert link into nav
-//   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-// }
-
-//   // currentLink?.classList.add("current");
-
-console.log("IT’S ALIVE!");
+console.log('IT’S ALIVE!');
 
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
+
+currentLink?.classList.add("current");
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const fallbackLink = navLinks.find(link => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+  return linkPage === currentPage;
+});
+
+// if (!currentLink && fallbackLink) {
+//   fallbackLink.classList.add("current");
+// }
+
 
 let pages = [
   { url: '', title: 'Home' },
@@ -70,38 +32,32 @@ let pages = [
   { url: 'https://github.com/netflyx', title: 'GitHub' }
 ];
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/netflyx/";         // GitHub Pages repo name
-
 let nav = document.createElement('nav');
 document.body.prepend(nav);
+
+if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
+}
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
+  // next step: create link and add it to nav
 
-  // Fix relative links for GitHub Pages
-  url = !url.startsWith('http') ? BASE_PATH + url : url;
-
-  // Create and insert link into nav
+  // Create link and add it to nav
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+
 }
 
-// Highlight logic after nav is created:
-const navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-
-// Optional fallback match by filename
-if (!currentLink) {
-  const currentPage = location.pathname.split("/").pop();
-  currentLink = navLinks.find(link => {
-    const linkPage = link.getAttribute("href").split("/").pop();
-    return linkPage === currentPage;
-  });
+if (!url.startsWith('http')) {
+  url = BASE_PATH + url;
 }
 
-currentLink?.classList.add("current");
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/netflyx/";         // GitHub Pages repo name
+
+console.log("Script is running");
+console.log("BASE_PATH is:", BASE_PATH);
+console.log("Body contents:", document.body.innerHTML);
+
