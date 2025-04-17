c9f1cc1ae1482f9512739fa63577ec5d91b26059
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-16 19:18:30 -0700

    edited global.js

--- global.js
+++ global.js
@@ -3,3 +3,22 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+const navLinks = $$("nav a");
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
+
+currentLink?.classList.add("current");
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const fallbackLink = navLinks.find(link => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+  return linkPage === currentPage;
+});
+
+// if (!currentLink && fallbackLink) {
+//   fallbackLink.classList.add("current");
+// }
