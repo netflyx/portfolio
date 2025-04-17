d67e8a96d1881f04b65bfbc0196dadbb0082ca27
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-17 14:54:28 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,24 +4,9 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-
-currentLink?.classList.add("current");
-
-const currentPage = window.location.pathname.split("/").pop();
-
-const fallbackLink = navLinks.find(link => {
-  const linkPage = link.getAttribute("href").split("/").pop();
-  return linkPage === currentPage;
-});
-
-// if (!currentLink && fallbackLink) {
-//   fallbackLink.classList.add("current");
-// }
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/netflyx/";         // GitHub Pages repo name
 
 
 let pages = [
@@ -44,20 +29,31 @@ for (let p of pages) {
   let title = p.title;
   // next step: create link and add it to nav
 
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
   // Create link and add it to nav
   nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 
 }
 
-if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
+const navLinks = $$("nav a");
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/netflyx/";         // GitHub Pages repo name
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname,
+  );
 
-console.log("Script is running");
-console.log("BASE_PATH is:", BASE_PATH);
-console.log("Body contents:", document.body.innerHTML);
+currentLink?.classList.add("current");
+
+const currentPage = window.location.pathname.split("/").pop();
+
+const fallbackLink = navLinks.find(link => {
+  const linkPage = link.getAttribute("href").split("/").pop();
+  return linkPage === currentPage;
+});
+
+// if (!currentLink && fallbackLink) {
+//   fallbackLink.classList.add("current");
+// }
 
