7779baa731430ad1e68627558a5ee010d2aeed24
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-18 12:03:33 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,56 +4,39 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-  ? "/"                  // Local server
-  : "/netflyx/";         // GitHub Pages repo name
-
-
 let pages = [
   { url: '', title: 'Home' },
   { url: 'contact/', title: 'Contact' },
   { url: 'projects/', title: 'Projects' },
-  { url: 'cv.html', title: 'CV' },
-  { url: 'https://github.com/netflyx', title: 'GitHub' }
+  { url: 'https://github.com/vdanielb', title: 'GitHub' },
+  { url: 'resume/', title: 'Resume' },
 ];
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
-if (!url.startsWith('http')) {
-  url = BASE_PATH + url;
-}
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/html-practice/";         // GitHub Pages repo name
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
-  // next step: create link and add it to nav
-
   if (!url.startsWith('http')) {
     url = BASE_PATH + url;
   }
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  nav.append(a);
+  
+  // detect current page
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
 
-  // Create link and add it to nav
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-
+  // all external links go to new tab
+  if (a.host !== location.host) { 
+    a.target = '_blank';
+  } 
 }
-
-const navLinks = $$("nav a");
-
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname,
-  );
-
-currentLink?.classList.add("current");
-
-const currentPage = window.location.pathname.split("/").pop();
-
-const fallbackLink = navLinks.find(link => {
-  const linkPage = link.getAttribute("href").split("/").pop();
-  return linkPage === currentPage;
-});
-
-// if (!currentLink && fallbackLink) {
-//   fallbackLink.classList.add("current");
-// }
-
