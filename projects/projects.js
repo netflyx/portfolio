94c6766743fabed33ba3aaeae4ef7fe315dcfd5d
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-25 15:19:05 -0700

    trying

--- projects/projects.js
+++ projects/projects.js
@@ -5,5 +5,3 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
-
-// test comment
