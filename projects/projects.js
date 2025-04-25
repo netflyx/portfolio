48de680e913198ed4e67d51d8ab009b17a74cc2e
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-25 13:45:16 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -5,10 +5,3 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
-
-{
-    "title": "NEW TITLE TEST",
-    "img": "https://vis-society.github.io/labs/2/images/empty.svg",
-    "description": "This is a new description to test dynamic updating!"
-  }
-  
