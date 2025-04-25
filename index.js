51e1a349a0bdcb420fbd2190b0e2b6e7944d6d55
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-04-25 16:40:06 -0700

    edited until end of part 3

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import { fetchGitHubData } from './global.js';
 
 const githubData = await fetchGitHubData('netflyx');
+
+const profileStats = document.querySelector('#stats-container');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
