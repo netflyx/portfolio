214ffd31c0e25f7323c19a493411b7b2374a82d9
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-05-01 15:20:56 -0700

    1.3?

--- projects/projects.js
+++ projects/projects.js
@@ -1,4 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 
 async function initProjects() {
   const projects = await fetchJSON('../lib/projects.json');
@@ -10,3 +12,11 @@ async function initProjects() {
   renderProjects(projects, projectsContainer, 'h2');
 }
 initProjects();
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
