9cf289e11c2c5bc8538cac50234e9815d5c09699
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-05-02 12:31:50 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -14,14 +14,22 @@ async function initProjects() {
 initProjects();
 
 
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
