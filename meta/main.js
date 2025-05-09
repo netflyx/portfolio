c4ea7c21a19194403ff1e31fed6cbea481aa7c6b
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-05-09 13:13:39 -0700

    1.3

--- meta/main.js
+++ meta/main.js
@@ -8,42 +8,87 @@ async function loadData() {
     length: Number(row.length),
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
+    file: row.file,
+    commit: row.commit,
+    author: row.author,
   }));
 
   return data;
 }
 
-
 function processCommits(data) {
-    return d3
-      .groups(data, (d) => d.commit)
-      .map(([commit, lines]) => {
-        let first = lines[0];
-        let { author, date, time, timezone, datetime } = first;
-  
-        let ret = {
-          id: commit,
-          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
-          author,
-          date,
-          time,
-          timezone,
-          datetime,
-          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
-          totalLines: lines.length,
-        };
-  
-        Object.defineProperty(ret, 'lines', {
-          value: lines,
-          writable: false,
-          configurable: false,
-          enumerable: false,
-        });
-  
-        return ret;
-      });
-  }
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
 
-  let data = await loadData();
-  let commits = processCommits(data);
-  console.log(commits);
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        writable: false,
+        configurable: false,
+        enumerable: false,
+      });
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Total commits
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  // Number of files
+  dl.append('dt').text('Number of files');
+  dl.append('dd').text(d3.groups(data, d => d.file).length);
+
+  // Max depth
+  dl.append('dt').text('Maximum depth');
+  dl.append('dd').text(d3.max(data, d => d.depth));
+
+  // Average file length
+  const fileLengths = d3.rollups(
+    data,
+    v => d3.max(v, d => d.line),
+    d => d.file
+  );
+  const avgFileLength = d3.mean(fileLengths, d => d[1]);
+
+  dl.append('dt').text('Average file length');
+  dl.append('dd').text(avgFileLength.toFixed(2));
+
+  // Most work time period
+  const workByPeriod = d3.rollups(
+    data,
+    v => v.length,
+    d => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+  );
+  const maxPeriod = d3.greatest(workByPeriod, d => d[1])?.[0];
+
+  dl.append('dt').text('Most work done during');
+  dl.append('dd').text(maxPeriod);
+}
+
+let data = await loadData();
+let commits = processCommits(data);
+
+renderCommitInfo(data, commits);
