a21900d9dd2ee671a2883f912a1c2f2e7e0e75a5
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-05-09 13:26:24 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -12,7 +12,6 @@ async function loadData() {
     commit: row.commit,
     author: row.author,
   }));
-
   return data;
 }
 
@@ -49,23 +48,18 @@ function processCommits(data) {
 function renderCommitInfo(data, commits) {
   const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
-  // Total LOC
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
 
-  // Total commits
   dl.append('dt').text('Total commits');
   dl.append('dd').text(commits.length);
 
-  // Number of files
   dl.append('dt').text('Number of files');
   dl.append('dd').text(d3.groups(data, d => d.file).length);
 
-  // Max depth
   dl.append('dt').text('Maximum depth');
   dl.append('dd').text(d3.max(data, d => d.depth));
 
-  // Average file length
   const fileLengths = d3.rollups(
     data,
     v => d3.max(v, d => d.line),
@@ -76,7 +70,6 @@ function renderCommitInfo(data, commits) {
   dl.append('dt').text('Average file length');
   dl.append('dd').text(avgFileLength.toFixed(2));
 
-  // Most work time period
   const workByPeriod = d3.rollups(
     data,
     v => v.length,
@@ -89,39 +82,60 @@ function renderCommitInfo(data, commits) {
 }
 
 function renderScatterPlot(data, commits) {
-    const width = 1000;
-    const height = 600;
-  
-    const svg = d3
-      .select('#chart')
-      .append('svg')
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .style('overflow', 'visible');
-  
-    const xScale = d3
-      .scaleTime()
-      .domain(d3.extent(commits, (d) => d.datetime))
-      .range([0, width])
-      .nice();
-  
-    const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
-  
-    const dots = svg.append('g').attr('class', 'dots');
-  
-    dots
-      .selectAll('circle')
-      .data(commits)
-      .join('circle')
-      .attr('cx', (d) => xScale(d.datetime))
-      .attr('cy', (d) => yScale(d.hourFrac))
-      .attr('r', 5)
-      .attr('fill', 'steelblue');
-  }
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
 
-  
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, d => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+    .selectAll('circle')
+    .data(commits)
+    .join('circle')
+    .attr('cx', d => xScale(d.datetime))
+    .attr('cy', d => yScale(d.hourFrac))
+    .attr('r', 5)
+    .attr('fill', 'steelblue');
+
+  svg
+    .append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(d3.axisBottom(xScale));
+
+  svg
+    .append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(d3.axisLeft(yScale).tickFormat(d => String(d % 24).padStart(2, '0') + ':00'));
+}
+
 let data = await loadData();
 let commits = processCommits(data);
 
 renderCommitInfo(data, commits);
 renderScatterPlot(data, commits);
-
