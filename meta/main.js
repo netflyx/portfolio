7be97947649053b037a177e7d002173859e3d04a
Alyx Wikarsa <alyx.wikarsa@gmail.com>
2025-05-09 12:37:53 -0700

    edited meta 1.2

--- meta/main.js
+++ meta/main.js
@@ -3,9 +3,9 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
     ...row,
-    line: Number(row.line),       // convert to number
-    depth: Number(row.depth),     // convert to number
-    length: Number(row.length),   // convert to number
+    line: Number(row.line),
+    depth: Number(row.depth),
+    length: Number(row.length),
     date: new Date(row.date + 'T00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
@@ -13,4 +13,37 @@ async function loadData() {
   return data;
 }
 
-let data = await loadData();
+
+function processCommits(data) {
+    return d3
+      .groups(data, (d) => d.commit)
+      .map(([commit, lines]) => {
+        let first = lines[0];
+        let { author, date, time, timezone, datetime } = first;
+  
+        let ret = {
+          id: commit,
+          url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+          author,
+          date,
+          time,
+          timezone,
+          datetime,
+          hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+          totalLines: lines.length,
+        };
+  
+        Object.defineProperty(ret, 'lines', {
+          value: lines,
+          writable: false,
+          configurable: false,
+          enumerable: false,
+        });
+  
+        return ret;
+      });
+  }
+
+  let data = await loadData();
+  let commits = processCommits(data);
+  console.log(commits);
